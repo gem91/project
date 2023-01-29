1695d61721be8eb5eeca0edde6673daa83f3b25f
mjkim <mjkim@ipartners.co.kr>
2023-01-29 23:56:45 +0900

    intro Home 페이지 생성

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './Home.css';
+import Header from "./components/Header";
+import AboutPage from './components/AboutPage';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <AboutPage />
+    </div>
+  );
+}
+
+export default Home;
