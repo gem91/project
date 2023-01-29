52269443a0a591efd42ef79ee46125f735eb921b
mjkim <mjkim@ipartners.co.kr>
2023-01-29 23:57:31 +0900

    메인에 필요한 기본페이지 생성

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = (props) => (
+  <header>
+    <h1>logo</h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
