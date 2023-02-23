0f95e3a18994a4df45656ae8c840e802f9cc8360
김민정 <mjkim@ipartners.co.kr>
2023-02-23 17:40:05 +0900

    404페이지 컴포넌트

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   return (
-    <div>
-      <p className="title">not found</p> 
+    <div className={styles.notFoundWrap}>
+      <div className={styles.container}>
+         <p>
+          Oooppss..!
+          <span>404 No Found Page</span>
+        </p>
+      </div>
     </div>
   );
 };
