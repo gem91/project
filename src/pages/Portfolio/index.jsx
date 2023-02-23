52893dff24a17d635c3220835c795117540cb16f
김민정 <mjkim@ipartners.co.kr>
2023-02-23 17:42:05 +0900

    project 컴포넌트 레이아웃 구성

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Portfolio.module.scss';
 import BreadCrumbs from '../../components/BreadCrumbs';
+import DefaultButton from '../../components/Button/DefaultButton';
 
-const Portfolio = (props) => (
+const Portfolio = (props) => {
+  const [src, setSrc] = useState('')
+  const fullImgRef = useRef(null)
+
+  useEffect(()=>{
+    console.log(fullImgRef);
+  })
+
+  const changeImg = (e) => {
+    const target = e.currentTarget;
+    const {src} = target
+      setSrc(src)
+      fullImgRef.setAttribute("src", src);
+      // fullImg.setAttribute("src", imgAttr);
+
+  }
+
+  return (
     <div className={styles.portfolio}>
-      <div class={styles.container}>
+      <div className={styles.container}>
          <BreadCrumbs text={'Project'}></BreadCrumbs>
-        <p className="title">
-        </p>
+        <div className={styles.projectWrap}>
+          <div className={styles.cardWrap}>
+            <div className={styles.cardCont}>
+              <div className={styles.contentLeft}>
+                  <img ref={fullImgRef} className={styles.fullImg} src="https://images.unsplash.com/photo-1592431454781-ec4870757ce9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Unsplash" />
+              </div>
+              <div className={styles.contentRight}>
+                  <div className={styles.button}>
+                    <DefaultButton type={'Go To Site'}></DefaultButton>
+                  </div>
+                  <div className={styles.textBox}>
+                    <h3 className={styles.title}>Outdoor Experience</h3>
+                    <p>It’s windy. The cool breeze of the lake. It gives, a sense of beauty, in motion. All is flowing, rushing and tide-And I sit in wonder, dreaming beside.</p>
+                  </div>
+                  <div className={styles.miniImgs}>
+                      <img className={styles.miniImg} src="https://images.unsplash.com/photo-1593346279196-b971a8e46966?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1974&q=80" alt="Unsplash" onClick={changeImg} />
+                      <img className={styles.miniImg} src="https://images.unsplash.com/photo-1594884592479-1f5bad0b696d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2002&q=80" alt="Unsplash" onClick={changeImg} />
+                      <img className={styles.miniImg} src="https://images.unsplash.com/photo-1592431454781-ec4870757ce9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Unsplash" onClick={changeImg} />
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
+};
 
 export default Portfolio;
