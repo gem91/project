c363abca294e5d8eeea5bf04d168c22a6e036378
김민정 <mjkim@ipartners.co.kr>
2023-02-28 18:04:21 +0900

    프로젝트 페이지 디자인 작업 및 데이터 수정 진행중

--- src/components/BlogCard/index.jsx
+++ src/components/BlogCard/index.jsx
@@ -2,25 +2,59 @@ import React from 'react';
 
 import styles from './BlogImage.module.scss';
 
-const BlogCard = ({imgData,mainImg, setMainImg, setMainAlt, fullImgRef}) => {
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/scss";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { Mousewheel, Navigation, Pagination } from "swiper";
+
+
+
+const BlogCard = ({imgData,mainImg, setMainImg, setMainAlt, fullImgRef, setTitle, setDesc, setLink}) => {
   const handleCardImg = (e) => {
     const target = e.currentTarget;
     const { src, alt } = target
     if (src === mainImg) return;
     fullImgRef.current.setAttribute('src', setMainImg(src))
     fullImgRef.current.setAttribute('alt', setMainAlt(alt))
+
+    imgData.map(({title,desc, srcURL, site}) => {
+      if( src === srcURL ){
+        setTitle(title)
+        setDesc(desc)
+        setLink(site)
+      }
+    })
   }
 
   return (
-    <ul className={styles.imgLists}>
-      {
-        imgData.map(({src, alt}, idx) => (
-          <li key={idx}>
-            <img src={src}alt={alt} onClick={handleCardImg} />
-          </li>
-        ))
-      }
-    </ul>
+    <div className={styles.imgLists}>
+      <Swiper
+       navigation={true}
+        slidesPerView={4}
+        spaceBetween={10}
+        mousewheel={true}
+        // scrollbar={{
+        // hide: false,
+        // }}
+        speed={2000}
+        modules={[ Mousewheel, Navigation, Pagination, ]}
+      >
+      { imgData.map(({srcURL, alt}, idx) => (
+        <SwiperSlide key={idx} >
+          <span className={styles.thumb}>
+            <img src={srcURL}alt={alt} onClick={handleCardImg} />
+          </span>
+        </SwiperSlide>
+      ))}
+      </Swiper> 
+    </div>
+
+  
   )
 };
 
--- src/components/Button/DefaultButton.jsx
+++ src/components/Button/DefaultButton.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import styles from './Button.module.scss'
 
 
-const DefaultButton = ({type = 'button', role = 'basic'} ) => {
+const DefaultButton = ({type = 'button', role = 'basic', setIsToggle} ) => {
+
+   const handleToggle = (e) => {
+      setIsToggle(true)
+    }
+    const test = (e) => {
+      console.log('test');
+    }
   return (
-    <button className={`${styles.defaultButton} ${styles[role]}`}>{type}</button>
+    <>
+    { setIsToggle && 
+      <button className={`${styles.defaultButton} ${styles[role]}`} onClick={handleToggle}>{type}</button>
+    }
+    { !setIsToggle &&
+      <button className={`${styles.defaultButton} ${styles[role]}`} onClick={test}>{type}</button>
+    }
+  </>
   )
 };
 
