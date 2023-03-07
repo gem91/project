bebf57a6c0a65b2334b476063950ea3808b5b85a
mjkim <mjkim@ipartners.co.kr>
2023-03-08 02:03:12 +0900

    인스타 좋아요 효과 추가

--- src/components/Instagram/index.jsx
+++ src/components/Instagram/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Instagram.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faHeart} from '@fortawesome/free-solid-svg-icons'
@@ -6,8 +6,14 @@ import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
 import {faInstagram} from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'react-router-dom';
 
-const Instagram = (props) => (
-  <div className={styles.profile}>
+const Instagram = (props) => {
+  const [ count, setCount ] = useState(0)
+  const getLikstCount = () => {
+    setCount(count + 1)
+    console.log(count);
+  }
+  return (
+    <div className={styles.profile}>
     <div className={styles.thumb}>
       <img src={`http://placeimg.com/600/700/any`} alt='민정이 사진' />
     </div>
@@ -23,12 +29,13 @@ const Instagram = (props) => (
           </a> */}
         </div>
         <div className={styles.ux}>
-        <Link to='/contact'>
-          <i className={styles.message}><FontAwesomeIcon icon={faEnvelope} /><span className='ir-hidden'>연락하기</span></i>
-        </Link>
-        <button type="button">
-          <i className={styles.like}><FontAwesomeIcon icon={faHeart} /><span className='ir-hidden'>좋아요</span></i>
-        </button>
+          <Link to='/contact'>
+            <i className={styles.message}><FontAwesomeIcon icon={faEnvelope} /><span className='ir-hidden'>연락하기</span></i>
+          </Link>
+          <button type="button" onClick={getLikstCount}>
+            <i className={styles.like}><FontAwesomeIcon icon={faHeart} /><span className='ir-hidden'>좋아요</span></i>
+          </button>
+          <div className={styles.likeEffect}><i className={styles.like}><FontAwesomeIcon icon={faHeart} /><span>{count}</span></i></div>
         </div>					
       </div>
       <p className={styles.text}>
@@ -36,6 +43,7 @@ const Instagram = (props) => (
       </p>
     </div>
   </div>
-  );
+  )
+};
 
 export default Instagram;
