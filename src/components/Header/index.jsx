524776f13bf902fac095b96ca3161c3206b627d1
김민정 <mjkim@ipartners.co.kr>
2023-03-13 17:03:28 +0900

    sns 아이콘, scroll 효과 추가

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,28 +4,44 @@ import LineFlow from '../FlowText/LineFlow';
 import Logo from '../Logo';
 import styles from './header.module.scss'
 
-const Header = ({isMobile}) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
+import InstaIcon from '../IconItems/InstaIcon';
+import KakaoIcon from '../IconItems/KakaoIcon';
+import MessageIcon from '../IconItems/MessageIcon';
+
+const Header = ({isMobile, isPc}) => {
   let [processBar, setProcessBar] = useState(0);
   const [isOpen, setIsOpen] = useState(false)
+  const [isHidden, setIsHidden] = useState(false)
+  const [prevY, setPrevY] = useState(0)
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-  }, []); // useEffect
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [prevY]); // useEffect
 
   const handleScroll = () => {
     let currY = window.scrollY     // 현재 스크롤 위치값
     const windowH = window.innerHeight   // 윈도우 화면 높이값
+    const isScrollUp = currY < prevY;
+
     ScrollNavBar(currY, windowH) // 가로 스크롤 네비바 기능 221026
-    
-    // const header = document.querySelector('header')
-    // if(currY > 100){
-    //   header.style.background = 'rgb(255 255 255 / 90%)'
-    // }else{
-    //   header.style.background = 'transparent'
-    // }
+    toggleHeader(isScrollUp);
+
+    setPrevY(currY) // prevY에 현재 스크롤 삽입
   };
-  const onHamburger = (e) => {
-    setIsOpen(!isOpen)
+
+
+  const onHamburger = (e) => setIsOpen(!isOpen)
+
+  const toggleHeader = (isToggleOn) => {    
+    isToggleOn
+    ? setIsHidden(false)
+    : setIsHidden(true)
   }
 
   const ScrollNavBar = (currrentY, windowH) => {
@@ -34,30 +50,35 @@ const Header = ({isMobile}) => {
     setProcessBar(processBar + scrollPercent)
   }
   return (
-    <header>
-      
-      { !isOpen && <div className={styles.processBar} style={{width: processBar + '%'}} onScroll={handleScroll} ></div> }
+    <header className={`${isHidden ? styles.isHidden : ''}`}>
+      { !isOpen && <div className={styles.processBar} style={{width: processBar + '%'}}  ></div> }
       <div className={styles.container}>
-        <Logo src={'/'} />
-        { !isMobile && 
-          <ul>
-            <li>
-              <Link to="/">About Me</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">Web Site</Link>  
-            </li>
-            <li>
-              <Link to="/portfolio">Toy Project</Link>  
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>  
-            </li>
-            {/* <li>
-              <Link to="/contact">Contact</Link>  
-            </li> */}
-        </ul>
-      }
+        <nav>
+          <Logo src={'/'} />
+          { !isMobile && 
+            <ul className={styles.gnb}>
+              <li>
+                <Link to="/">About Me</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">Web Site</Link>  
+              </li>
+              <li>
+                <Link to="/toy">Toy Project</Link>  
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>  
+              </li>
+          </ul>
+        }
+        { isPc && 
+          <ul className={styles.sns}>
+            <li><InstaIcon /></li>
+            <li><KakaoIcon /></li>
+            <li><MessageIcon /></li>
+          </ul>
+        }
+        </nav>
       </div>
       { isMobile && 
         <button className={ isOpen ? `${styles.hamburger} ${styles.isOpen}` :  `${styles.hamburger}`} type='button' onClick={onHamburger}>
